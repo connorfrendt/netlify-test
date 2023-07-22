49bcada3009e2cbbdacdf6b1c51d1a65120fb506
Connor Frendt <cjacksonfx@yahoo.com>
2023-07-22 11:44:24 -0700

    another test commit

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
         This is the main component!
+        THIS IS WORKING!!! :D
+        <div id="press" v-bind:on-click="pressMe">PRESS ME!</div>
     </div>
 </template>
 
 <script>
-
+export default {
+    methods: {
+        pressMe() {
+            console.log('I am pressed!');
+        }
+    }
+}
 </script>
 
 <style>
-
+#press {
+    background-color: red;
+}
 </style>
